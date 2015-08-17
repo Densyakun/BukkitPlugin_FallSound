9e80555475c2aae874def1dcfdff59198eaefa65
電車君 <Densyakun2001@excite.co.jp>
2015-08-18 04:12:31 +0900

    Update Main.java
    修正しました

--- src/org/densyakun/bukkit/fs/Main.java
+++ src/org/densyakun/bukkit/fs/Main.java
@@ -1,4 +1,4 @@
-package org.densyakun.bukkit.fs;
+package org.densyakun.bukkit.fallsound;
 import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -7,12 +7,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.plugin.java.JavaPlugin;
-public class Main extends JavaPlugin implements Listener{
-	public void onEnable(){
-		this.getServer().getPluginManager().registerEvents(this,this);
+public class Main extends JavaPlugin implements Listener {
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 	@EventHandler
-	public void EntityDamage(EntityDamageEvent e){
-		if((e.getEntityType()==EntityType.PLAYER)&&(e.getCause()==DamageCause.FALL))((Player)e.getEntity()).playSound(((Player)e.getEntity()).getLocation(),Sound.HURT,1,0);
+	public void EntityDamage(EntityDamageEvent e) {
+		if ((e.getEntityType()==EntityType.PLAYER) && (e.getCause()==DamageCause.FALL)) {
+			((Player) e.getEntity()).playSound(((Player) e.getEntity()).getLocation(), Sound.HURT, 1, 0);
+		}
 	}
 }
